a0813722fab19fdeeaa3a58fa10a59b63366a042
camrevalo <xmeelo@gmail.com>
2017-06-21 15:44:27 -0700

    life is beautiful so is this new code

--- server.js
+++ server.js
@@ -9,14 +9,39 @@ app.use(bodyParser.urlencoded({
 
 var port = process.env.PORT || 8080;
 
-var token = "<YOUR_APP_TOKEN>";
+var token = "EAAEe2ztWUL4BAP6gZAHvqr06oHZBqoZBQwL2HaHtrbt45V00g2mcZB3wtUgJuEd2nZAqviYmZB5ZA7fv3fJiAubZAVn2bzb2xPkAXvH3ZAkoLcfJfiNEnseCqnoSb4i8kepBNEplC3umwqCtsbBh19KmS7F5UYGJMhKFFPKN1xejwPAZDZD";
+
+
 
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '<YOUR_VERIFY_TOKEN>') {
+  if (req.query['hub.verify_token'] === 'artemis_token_2009') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
+
+var  math = require('mathjs');
+
+function replyToSender(sender, text) {
+  messageData = {
+    text : text
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token : token },
+    method: 'POST',
+    json: {
+       recipient: { id : sender },
+       message: messageData,
+    }
+  }, function(error, response, body) {
+       if (error) {
+         console.log('Error sending message: ', error);
+       } else if (response.body.error) {
+         console.log('Error: ', response.body.error);
+       }
+     });
+};
 
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
